45dfccfbf19ddcad523a96888602b2f09e888ff5
pal4de <pal4de@gmail.com>
2021-07-18 22:24:24 +0900

    フロントエンドを少し実装

--- app.ts
+++ app.ts
@@ -1,26 +1,29 @@
 import express from 'express';
-const app = express();
-
 import Web3 from 'web3';
 import { AbiItem } from "web3-utils";
 import { PaidHTTP } from './types/web3-v1-contracts/PaidHTTP';
 
+const app = express();
+app.set('view engine', 'ejs');
+
 const provider = 'http://127.0.0.1:8545';
-const paidhttpAddress = '0x6d57cCd4F3f52fAB1103b178cdE0f2aBF95cc5D8';
-const ownerAddress = '0x93074Ec6AB154eE02Ec294FD3aADd291789d3796';
+const constructInfo = require('./build/contracts/PaidHTTP.json')
+const constructAddr = '0x6d57cCd4F3f52fAB1103b178cdE0f2aBF95cc5D8';
+const ownerAddr = '0x93074Ec6AB154eE02Ec294FD3aADd291789d3796';
 
 const web3 = new Web3(new Web3.providers.HttpProvider(provider));
-const paidhttpAbi = require('./build/contracts/PaidHTTP.json').abi as AbiItem;
-const contract = new web3.eth.Contract(paidhttpAbi, paidhttpAddress) as unknown as PaidHTTP;
+const paidhttpAbi = constructInfo.abi as AbiItem;
+const contract = new web3.eth.Contract(paidhttpAbi, constructAddr) as unknown as PaidHTTP;
 
-app.use(async (request: express.Request, response: express.Response) => {
-    const path = request.originalUrl;
-
-    const price = await contract.methods.getFee(path).call({from: ownerAddress});
-    response.status(200).send(`${path}: ${price}`);
-
-    const newPrice = parseInt(price) + 100;
-    await contract.methods.setFee(path, newPrice).send({from: ownerAddress});
+app.use(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const data = {
+        paid: false
+    };
+    if (!data.paid) {
+        res.status(402);
+    }
+    res.render('./index.ejs', data);
 });
 
+console.log('ready');
 app.listen(3000);
