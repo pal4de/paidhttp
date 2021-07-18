1f8934ae47238b976a20ecf9a21904228a55cbe2
pal4de <pal4de@gmail.com>
2021-07-18 19:44:39 +0900

    訪問するごとに価格が上昇

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const Web3 = require('web3');
 const app = express();
 
 const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
-const address = '0xE3B3D5BfFb680Df948545Ce74F42f76b04C47980';
-const abi = require('./build/contracts/PaidHTTP.json').abi;
-const contract = new web3.eth.Contract(abi, address);
+const paidhttpAddress = '0x6d57cCd4F3f52fAB1103b178cdE0f2aBF95cc5D8';
+const paidhttpAbi = require('./build/contracts/PaidHTTP.json').abi;
+const contract = new web3.eth.Contract(paidhttpAbi, paidhttpAddress);
 
-contract.methods.setFee('/', 100).send({from: '0x93074Ec6AB154eE02Ec294FD3aADd291789d3796'});
+const ownerAddress = '0x93074Ec6AB154eE02Ec294FD3aADd291789d3796';
+
+let price = 100;
 
 app.get('/', async (request, response) => {
-    const res = await contract.methods.getFee('/').call({from: '0x93074Ec6AB154eE02Ec294FD3aADd291789d3796'});
+    await contract.methods.setFee('/', price).send({from: ownerAddress});
+    const res = await contract.methods.getFee('/').call({from: ownerAddress});
     response.status(200).send(res);
+    price += 100;
 });
 
 app.listen(3000);
